e96cd4c06008f3028c425bdad72d45442bf63300
wozny <maciej.wozny@nokia.com>
2016-11-16 23:02:39 +0100

    Adding receive json and show message

--- notifications/app/controllers/HomeController.java
+++ notifications/app/controllers/HomeController.java
@@ -1,14 +1,31 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import play.mvc.*;
 
 import views.html.*;
-
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+
+    String notification = "";
+
+    public Result receive() {
+        JsonNode json = request().body().asJson();
+        if (json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            notification = json.findPath("message").textValue();
+            if (notification == null) {
+                notification = "Empty";
+                return badRequest("Missing parameter [message]");
+            } else {
+                return ok("Your last message: " + notification);
+            }
+        }
+    }
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -17,7 +34,6 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Your message: " + notification);
     }
-
 }
