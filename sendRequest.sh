61501374199da1c7b9282a6b976f78301137504c
Kuba Tumidajewicz <kubat26@gmail.com>
2016-11-17 00:20:36 +0100

    sending message script

---
+++ sendRequest.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+while [ : ]
+do
+	time=$(date +"%T")
+	curl --header "Content-type: application/json" -X POST --data "{\"message\":\""${time}"\n\"}" http://localhost:9000
+	sleep 5
+done
