2800c42e2bd385fc572bdea98485ea0203a8c919
Kuba Tumidajewicz <kubat26@gmail.com>
2016-11-21 19:45:00 +0100

    updating microservice

--- data_microservice/Data_microservice/src/rabbitConnection/Deserialization.java
+++ data_microservice/Data_microservice/src/rabbitConnection/Deserialization.java
@@ -1,6 +1,25 @@
+package rabbitConnection;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.concurrent.TimeoutException;
 
 public class Deserialization {
+	private String queueName = "hello";
+	ReceiveFromRabbit rabbit = new ReceiveFromRabbit(queueName);
 	
-	
-
+	public Notification getNotificationObject(){
+		
+		try{
+			rabbit.getNotificationBytes();
+			ByteArrayInputStream in = new ByteArrayInputStream(rabbit.getNotification());
+			ObjectInputStream is = new ObjectInputStream(in);
+			return (Notification)is.readObject();
+		}catch (IOException | InterruptedException | TimeoutException | ClassNotFoundException e){
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		}
+		return null;
+	}
 }
